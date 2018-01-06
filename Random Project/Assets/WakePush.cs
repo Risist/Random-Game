37628ab411096d8826c5354b042f165e2bd39681
Risist <dmnn@op.pl>
2018-01-06 21:25:19 +0100

    post processing. Bomb. New map.

---
+++ Random Project/Assets/WakePush.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WakePush : MonoBehaviour {
+
+	public float powerMin = 0;
+	public float powerMax = 1;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D>().AddForce(Random.insideUnitCircle * Random.Range(powerMin, powerMax));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
