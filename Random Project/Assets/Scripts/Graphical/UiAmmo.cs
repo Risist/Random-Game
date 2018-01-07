7f996e6a7ba6dc1717c12dc5e3cf8ad5b5092e0e
Risist <dmnn@op.pl>
2018-01-07 19:54:00 +0100

    Room and ui. Totems

---
+++ Random Project/Assets/Scripts/Graphical/UiAmmo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiAmmo : MonoBehaviour {
+
+	Weapon playerAmmo;
+	Image image;
+
+	// Use this for initialization
+	void Start()
+	{
+		image = GetComponent<Image>();
+		playerAmmo = GameObject.FindGameObjectWithTag("Player").GetComponent<Weapon>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		image.fillAmount = playerAmmo.ammo / playerAmmo.maximumAmmo;
+	}
+}
