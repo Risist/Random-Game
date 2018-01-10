ff83f3d5709adff9fb4ee30a3959efc98b7a1520
Herti32 <34347666+Herti32@users.noreply.github.com>
2018-01-10 20:57:37 +0100

    Shield and shuriken

---
+++ Random Project/Assets/RotateShuriken.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateShuriken : MonoBehaviour
+{
+
+    public float rotationSpeed = 10;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.RotateAround(transform.position, Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+}
