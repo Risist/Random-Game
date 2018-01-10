562ac60e53da50d0e88cf154966326c049dbe0b2
Risist <dmnn@op.pl>
2018-01-10 16:27:52 +0100

    fsdfsafs

---
+++ Random Project/Assets/pullInside.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pullInside : MonoBehaviour {
+
+	public float force;
+	public float forceIncrease;
+
+	private void Update()
+	{
+		force += forceIncrease * Time.deltaTime;
+	}
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		var rb = collision.GetComponent<Rigidbody2D>();
+		if (rb)
+		{
+			rb.AddForce(((Vector2)transform.position - rb.position).normalized * force);
+		}
+	}
+}
