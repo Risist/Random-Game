e82a6b91ad0a0cb1a1fb2fee0d7ebc204ed82fca
Risist <dmnn@op.pl>
2018-02-10 10:05:53 +0100

    Fixes

--- Random Project/Assets/Scripts/Skills/ProgressionManager.cs
+++ Random Project/Assets/Scripts/Skills/ProgressionManager.cs
@@ -32,6 +32,7 @@ public class ProgressionManager : MonoBehaviour {
 
 #region lvl
 	public int lvl;
+	public int leftSkillPoints;
 	float xp = 0.0f;
 	public float requiredXpBase = 1.0f;
 	public float requiredXpScale = 1.0f;
@@ -44,6 +45,7 @@ public class ProgressionManager : MonoBehaviour {
 		while(xp > GetRequiredXp() )
 		{
 			++lvl;
+			++leftSkillPoints;
 			xp -= GetRequiredXp();
 			BroadcastMessage("OnLvlUp", this);
 		}
@@ -67,10 +69,21 @@ public class ProgressionManager : MonoBehaviour {
 		[System.NonSerialized]
 		public ProgressionManager manager;
 	}
-	[System.NonSerialized]
-	public string[] possibleFateNames;
+	string[] possibleFateNames;
+	public string GetRandomFateName() { return possibleFateNames[Random.Range(0, possibleFateNames.Length)]; }
+	public string GetRandomLockedFateName()
+	{
+		List<string> names = new List<string>();
+		foreach (var it in possibleFateNames)
+			if (it != chosenFates[0].name && it != chosenFates[1].name)
+				names.Add(it);
+		return names[Random.Range(0, names.Count)];
+	}
+
 	public int maxFateLvl = 5;
 	public Fate[] chosenFates = new Fate[2];
+
+	public bool IsFateUnlocked(string name) { return chosenFates[0].name == name || chosenFates[1].name == name; }
 
 	// unlocks a fate with specified name
 	public bool UnlockFate(string name)
@@ -96,12 +109,14 @@ public class ProgressionManager : MonoBehaviour {
 		return false;
 	}
 
-	// lvl ups given fate. If fate has max lvl returns false instead; Returns false in case if fate is invalid or not choosen as a path
+	// lvl ups given fate. If fate has max lvl returns false instead; Returns false in case if fate is invalid or not choosen as a path or not having enough skillpoints
 	public bool LvlUpFate(int id) { return LvlUpFate(chosenFates[id]); }
 	public bool LvlUpFate(Fate fate)
 	{
-		if (fate == null)
+		if (fate == null || leftSkillPoints <= 1)
 			return false;
+
+		--leftSkillPoints;
 
 		if(fate.lvl < maxFateLvl)
 		{
@@ -147,9 +162,10 @@ public class ProgressionManager : MonoBehaviour {
 	public bool UnlockSkill(int id) { return UnlockSkill(possibleSkills[id]); }
 	public bool UnlockSkill(WeaponBase weapon)
 	{
-		if (IsUnlocked(weapon) || !weapon.MeetsRequirements())
+		if (leftSkillPoints <=1 || IsUnlocked(weapon) || !weapon.MeetsRequirements())
 			// the skill is already unlocked
 			// or skill cant be unlocked yet
+			// or not enough skillPoints left
 			return false;
 
 		unlockedSkills.Add(weapon);
