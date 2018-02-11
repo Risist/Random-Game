795dcbf6fe0275c222fff8e60b3a0fad392f1bc0
Risist <dmnn@op.pl>
2018-02-12 00:32:17 +0100

    Enemy&ally ai change Battle map test

--- Random Project/Assets/Scripts/Movement/PushOnCollision.cs
+++ Random Project/Assets/Scripts/Movement/PushOnCollision.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class PushOnCollision : MonoBehaviour {
 
 	public Vector2 force;
+	public bool ignoreTrigger = false;
 
 	private void OnTriggerStay2D(Collider2D collision)
 	{
+		if (ignoreTrigger && collision.isTrigger)
+			return;
+
 		Rigidbody2D rb = collision.GetComponent<Rigidbody2D>();
 		if(rb)
 			rb.AddForce(new Vector2(transform.up.x, transform.up.y) * force.y + 
