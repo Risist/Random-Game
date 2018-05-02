ab220445117b07d6363731f131cc9babcec293b2
Stalex89 <23526237+Stalex89@users.noreply.github.com>
2018-05-02 21:11:01 +0200

    fix for pad toggle, add ToggleWrapper script

---
+++ Random Project/Assets/Scripts/Resource/Ui/ToggleWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleWrapper : MonoBehaviour {
+
+    
+    public Toggle toggle;
+    public PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        toggle = GetComponent<Toggle>();
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        toggle.isOn = playerMovement.pad;
+        toggle.onValueChanged.AddListener(ListenerMethod);
+    }
+
+    public void ListenerMethod(bool value)
+    {
+        playerMovement.setPad(toggle.isOn);
+    }
+}
