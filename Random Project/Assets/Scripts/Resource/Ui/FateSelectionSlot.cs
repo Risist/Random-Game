1371b69c74147bd05c5ffec012b27b7a673dc91c
Stalex89 <23526237+Stalex89@users.noreply.github.com>
2018-05-02 11:30:59 +0200

    import updated canvas with dependencies

---
+++ Random Project/Assets/Scripts/Resource/Ui/FateSelectionSlot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class FateSelectionSlot : MonoBehaviour
+{
+    [SerializeField]
+    public ProgressionManager.Fate fate;
+    [SerializeField]
+    public Text fateName;
+    [SerializeField]
+    public Text level;
+    [SerializeField]
+    public Text description;
+    [SerializeField]
+    public Image icon;
+
+
+    private void Awake()
+    {
+
+        fateName = gameObject.transform.Find("Background").Find("FateNameText").GetComponent<Text>();
+        level = gameObject.transform.Find("Background").Find("FateLvlText").GetComponent<Text>();
+        description = gameObject.transform.Find("Background").Find("FateDescriptionText").GetComponent<Text>();
+        icon = gameObject.transform.Find("Background").Find("FateIconImage").GetComponent<Image>();
+
+    }
+
+}
